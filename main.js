b5495bd45c0b5dc8ce8d9264b892e81ec99aca1e
Михаил Севостьянов <ssmit58@gmail.com>
2024-08-19 13:55:34 +0300

    String repeat

--- main.js
+++ main.js
@@ -1,33 +1,11 @@
+// Write a
+// function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
 
-// I love you, a little , a lot, passionately ... not at all
-
-// Who remembers back to their time in the schoolyard, when girls would take a flower and tear its petals, saying each of the following phrases each time a petal was torn:
-
-//     "I love you"
-// "a little"
-// "a lot"
-// "passionately"
-// "madly"
-// "not at all"
-// If there are more than 6 petals, you start over with "I love you"
-// for 7 petals, "a little"
-// for 8 petals and so on.
-
-// When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
-
-// Your goal in this kata is to determine which phrase the girls would say at the last petal
-// for a flower of a given number of petals.The number of petals is always greater than 0.
-
-const phrases = [
-    'I love you',
-    'a little',
-    'a lot',
-    'passionately',
-    'madly',
-    'not at all',
-]
-
-function howMuchILoveYou(n) {
-    return phrases[(n - 1) % phrases.length]
+function repeatStr(n, s) {
+    let str = ''
+    for (let i = 0; i < n; i++) {
+        str += s
+    }
+    return str
 }
-console.log(howMuchILoveYou(6))
+console.log(repeatStr(4, 'привет'))
